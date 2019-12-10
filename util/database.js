bf5cd4eab22a8316567856c80d603a11d9d182a4
Oleg Sachok <oleg.sachok@dataart.com>
2019-12-10 14:51:35 +0200

    start working with mongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-udemy', 'root', 'admin', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://oleh:adminadmin@cluster0-b6rzg.mongodb.net/test?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('mongo connected');
+    callback(client);
+  })
+  .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect; 
